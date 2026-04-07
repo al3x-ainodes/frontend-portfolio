b929b0bdff24e3ee678aaae6867b76d808f75950
Yander <martinezyander@proton.me>
2026-04-06 23:07:31 -0400

    Add Vercel Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Experience />
       <Contact />
       <Footer />
+      <SpeedInsights />
     </div>
   );
 }
